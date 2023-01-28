14980c051fd824dc1ff7afd1d57d4367604343a9
Aaron Mathews <80647418+AaronMathews22@users.noreply.github.com>
2023-01-28 13:52:57 -0600

    Tried to add a template

--- app.py
+++ app.py
@@ -1,16 +1,7 @@
-#!/usr/bin/env python3
-
-from flask import Flask, render_template, jsonify, Response, request, redirect, url_for
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-    return render_template("index.html")
-
-@app.route("/postRequest", methods = ["POST"])
-
-@app.route("/getRequest", methods = ["GET"])
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=False, threaded=True)
+def startup():
+    return render_template('index.html')
--- hello.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route("/")
-
-def howdy_world():
-    return "<p>Howdy, World!</p>"
