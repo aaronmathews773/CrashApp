84082e7c2b274f0f73f35b917c166c7ed97b8415
Aaron Mathews <80647418+AaronMathews22@users.noreply.github.com>
2023-01-28 16:13:17 -0600

    Added pages for crash, emissions, and traffic data

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import streamlit as st
+import json
 
 st.write('Crash App: Drive Safer')
 
+x = st.slider('x')
+st.write(x, 'squared is', x*x)
 
 st.write('Created by Aaron Mathews and Caleb Lee for TAMU Hack 2023')
