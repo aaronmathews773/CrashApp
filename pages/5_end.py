21342611d09ae08ee2a8bf54be77cba3b6348139
Aaron Mathews <80647418+AaronMathews22@users.noreply.github.com>
2023-01-29 09:28:46 -0600

    Removed Charging Stations and updated end page

--- pages/5_end.py
+++ pages/5_end.py
@@ -4,4 +4,10 @@ st.title('Thanks!')
 
 st.header('Credits')
 st.write('Aaron Mathews - Title Page, Traffic Page, Emissions Page, End Page')
+
+st.subheader('Libraries Used')
+st.write('- Streamlit.io')
+
+st.subheader('APIs used')
+
 st.balloons()
