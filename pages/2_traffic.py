2b56b76a42026520958241a7df34398a75e5cf74
Aaron Mathews <80647418+AaronMathews22@users.noreply.github.com>
2023-01-29 10:46:38 -0600

    Finished traffic page

---
+++ pages/2_traffic.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import pandas as pd
+
+st.title('Traffic Data in San Jose')
+
+st.write('Just like how analyzing crash data can help prevent collisions, analyzing traffic data can hopefully lead to less traffic congestion. This could happen in terms of better road design, introduction of more buses, or something else.')
+
+data = pd.read_csv('Average_Daily_Traffic.csv')
+truncated_data = data.head(1000)
+
+st.map(truncated_data)
+
+st.write(truncated_data)
+
+st.write('Try out the slider below to see the average daily traffic counts in various intersections')
+street = st.select_slider('Slide to the intersection you want to analyze', truncated_data['NEARINTERS'])
+st.write(f':green[{truncated_data[truncated_data["NEARINTERS"] == street]["ADT"]}]')
---
+++ pages/4_end.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.title('Thanks!')
+
+st.subheader('Libraries Used')
+st.write('- Streamlit.io : https://streamlit.io/')
+st.write('- Pandas: https://pandas.pydata.org/')
+st.write('- Requests: https://pypi.org/project/requests/')
+
+st.subheader('APIs used')
+st.write('- https://emissions-api.org/')
+
+st.subheader('Datasets Used')
+st.write('- https://data.sanjoseca.gov/dataset/crashes-data')
+st.write('- https://data.sanjoseca.gov/dataset/average-daily-traffic1')
+
+st.balloons()
